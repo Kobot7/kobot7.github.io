938a7e6696f26e58c7f5c545246a55555419941b
Kobot7 <estherkojialing@gmail.com>
2021-01-06 01:51:34 +0800

    Refined stuff
    - Added individual project descriptions
    - Fixed formatting issues

--- packages/js/navbar.js
+++ packages/js/navbar.js
@@ -48,7 +48,7 @@ $(document).ready(function(){
   $("a").on('click', function(event) {
 
     // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
+    if (this.hash !== "" && ["#aspj-carousel", "#dcrypt-carousel", "#retrograde-carousel", "#appdev-carousel"].indexOf(this.hash) < 0) {
       // Prevent default anchor click behavior
       event.preventDefault();
 
